e3719f87e910de9f898e01c35cd713825c437a96
marcocaza <44465310+marcocaza@users.noreply.github.com>
2021-11-09 09:10:41 -0500

    Update and rename angular-app/src/app/about.component.ts to aplicación angular/src/aplicación/about.component.ts

--- aplicación angular/src/aplicación/about.component.ts
+++ aplicación angular/src/aplicación/about.component.ts
@@ -15,8 +15,7 @@ import { Component } from '@angular/core';
         <ul>
           <li>
             <iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=30c12151-8910-45b0-8814-f77e3fa05655&autoAuth=true&ctid=d394dfea-16f8-40e0-9e3d-491c68096f33&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXBhYXMtMS1zY3VzLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9" frameborder="0" allowFullScreen="true"></iframe>
-              Code in GitHub
-            </a>
+       
           </li>
         </ul>
       </div>
