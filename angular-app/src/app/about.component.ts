ef53c1ce392ede217c5c7f312a4126a47ff274d1
marcocaza <44465310+marcocaza@users.noreply.github.com>
2021-11-09 09:02:09 -0500

    Update about.component.ts

--- angular-app/src/app/about.component.ts
+++ angular-app/src/app/about.component.ts
@@ -14,7 +14,7 @@ import { Component } from '@angular/core';
         <h2 class="title">Resources</h2>
         <ul>
           <li>
-            <a href="https://github.com/MicrosoftDocs/mslearn-staticwebapp">
+            <iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=30c12151-8910-45b0-8814-f77e3fa05655&autoAuth=true&ctid=d394dfea-16f8-40e0-9e3d-491c68096f33&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXBhYXMtMS1zY3VzLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9" frameborder="0" allowFullScreen="true"></iframe>
               Code in GitHub
             </a>
           </li>
